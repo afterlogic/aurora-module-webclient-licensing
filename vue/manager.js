9cf2327f5b0d98a671ea3e7361a35546b629f183
nadine-shi <nadinshi@gmail.com>
2021-06-18 16:40:36 +0300

    requiredModules functionality

--- vue/manager.js
+++ vue/manager.js
@@ -1,14 +1,18 @@
 import settings from '../../LicensingWebclient/vue/settings'
 
 export default {
-  name: 'LicensingWebclient',
+  moduleName: 'LicensingWebclient',
+
+  requiredModules: ['Licensing'],
+
   init (appData) {
     settings.init(appData)
   },
+
   getAdminSystemTabs () {
     return [
       {
-        name: 'licensing',
+        tabName: 'licensing',
         title: 'LICENSINGWEBCLIENT.LABEL_LICENSING_SETTINGS_TAB',
         component () {
           return import('src/../../../LicensingWebclient/vue/components/LicensingAdminSettings')
